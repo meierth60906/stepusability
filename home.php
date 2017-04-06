709c62d65d38a90f23b1cee0b42599721db70b82
meierth60906 <meier.t96@gmail.com>
2017-04-06 22:12:07 +0200

    Nav responsive + Auslagerung in navigation.html + home.html wird zu home.php

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard | step</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+</head>
+<body>
+
+<?php include ('navigation.html'); ?>
+
+<section id="dashboard">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1>Step, the best tool to support usability testing.</h1>
+
+            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+        </div>
+    </div>
+</div>
+</section>
+
+<script src="js/jquery-3.2.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
