74c472c72aa5f81b64927cf18a84dcb066948dad
meierth60906 <meier.t96@gmail.com>
2017-04-07 12:05:52 +0200

    Dashboard: Projekte-Karte erstellt

--- home.php
+++ home.php
@@ -15,20 +15,76 @@
 <section id="dashboard" class="page-content">
 <div class="container-fluid">
     <div class="row titlebar px-5 py-3">
-        <div class="col-lg-10 align-self-center">
+        <div class="col-9 align-self-center">
             <h1>Home</h1>
 
             <!--            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>-->
         </div>
-        <div class="col-lg-2 text-right align-self-center">
+        <div class="col-3 text-right align-self-center">
             <div class="icon-cog d-inline"></div> Einstellungen
         </div>
     </div>
+</div>
+<div class="container">
     <div class="row">
         <div class="col-lg-6">
-            <div class="card m-5">
+            <div class="card my-3">
                 <div class="card-header red">
-                    Projekte
+                    <div class="icon-folder d-inline"></div> Projekte
+                </div>
+                <div class="card-block pre-scrollable pt-0">
+                    <div class="row card-item p-3">
+                        <div class="col-7">
+                            <div class="icon-folder d-inline"></div> Projekt 1
+                        </div>
+                        <div class="col-5 text-right">
+                            07.04.2017
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row card-item p-3">
+                        <div class="col-7">
+                            <div class="icon-folder d-inline"></div> Projekt 2
+                        </div>
+                        <div class="col-5 text-right">
+                            02.04.2017
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row card-item p-3">
+                        <div class="col-7">
+                            <div class="icon-folder d-inline"></div> Projekt 2
+                        </div>
+                        <div class="col-5 text-right">
+                            02.04.2017
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row card-item p-3">
+                        <div class="col-7">
+                            <div class="icon-folder d-inline"></div> Projekt 2
+                        </div>
+                        <div class="col-5 text-right">
+                            02.04.2017
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row card-item p-3">
+                        <div class="col-7">
+                            <div class="icon-folder d-inline"></div> Projekt 2
+                        </div>
+                        <div class="col-5 text-right">
+                            02.04.2017
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card my-3">
+                <div class="card-header red">
+                    <div class="icon-file-text d-inline"></div> Zuletzt geöffnete Dokumente
                 </div>
                 <div class="card-block text-center">
                     <div class="icon-folder"></div> Projekt 1
@@ -37,9 +93,9 @@
             </div>
         </div>
         <div class="col-lg-6">
-            <div class="card m-5">
+            <div class="card my-3">
                 <div class="card-header red">
-                    Projekte
+                    <div class="icon-calendar-1 d-inline"></div> Termine
                 </div>
                 <div class="card-block text-center">
                     <div class="icon-folder"></div> Projekt 1
@@ -48,20 +104,9 @@
             </div>
         </div>
         <div class="col-lg-6">
-            <div class="card m-5">
+            <div class="card my-3">
                 <div class="card-header red">
-                    Projekte
-                </div>
-                <div class="card-block text-center">
-                    <div class="icon-folder"></div> Projekt 1
-                </div>
-
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div class="card m-5">
-                <div class="card-header red">
-                    Projekte
+                    <div class="icon-star d-inline"></div> Favoriten
                 </div>
                 <div class="card-block text-center">
                     <div class="icon-folder"></div> Projekt 1
