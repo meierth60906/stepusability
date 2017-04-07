d8ea1803c2295f47abbab133c5933b9635022278
meierth60906 <meier.t96@gmail.com>
2017-04-07 13:05:07 +0200

    Dashboard: Hover-Effekte, Anpassungen, Dokument-Karte erstellt

--- home.php
+++ home.php
@@ -13,109 +13,153 @@
 <?php include ('navigation.html'); ?>
 
 <section id="dashboard" class="page-content">
-<div class="container-fluid">
-    <div class="row titlebar px-5 py-3">
-        <div class="col-9 align-self-center">
-            <h1>Home</h1>
+    <div class="container-fluid">
+        <div class="row titlebar px-5 py-3">
+            <div class="col-9 align-self-center">
+                <h1>Home</h1>
 
-            <!--            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>-->
-        </div>
-        <div class="col-3 text-right align-self-center">
-            <div class="icon-cog d-inline"></div> Einstellungen
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="card my-3">
-                <div class="card-header red">
-                    <div class="icon-folder d-inline"></div> Projekte
-                </div>
-                <div class="card-block pre-scrollable pt-0">
-                    <div class="row card-item p-3">
-                        <div class="col-7">
-                            <div class="icon-folder d-inline"></div> Projekt 1
-                        </div>
-                        <div class="col-5 text-right">
-                            07.04.2017
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row card-item p-3">
-                        <div class="col-7">
-                            <div class="icon-folder d-inline"></div> Projekt 2
-                        </div>
-                        <div class="col-5 text-right">
-                            02.04.2017
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row card-item p-3">
-                        <div class="col-7">
-                            <div class="icon-folder d-inline"></div> Projekt 2
-                        </div>
-                        <div class="col-5 text-right">
-                            02.04.2017
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row card-item p-3">
-                        <div class="col-7">
-                            <div class="icon-folder d-inline"></div> Projekt 2
-                        </div>
-                        <div class="col-5 text-right">
-                            02.04.2017
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row card-item p-3">
-                        <div class="col-7">
-                            <div class="icon-folder d-inline"></div> Projekt 2
-                        </div>
-                        <div class="col-5 text-right">
-                            02.04.2017
-                        </div>
-                    </div>
-                </div>
-
+                <!--            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>-->
             </div>
-        </div>
-        <div class="col-lg-6">
-            <div class="card my-3">
-                <div class="card-header red">
-                    <div class="icon-file-text d-inline"></div> Zuletzt geöffnete Dokumente
-                </div>
-                <div class="card-block text-center">
-                    <div class="icon-folder"></div> Projekt 1
-                </div>
-
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div class="card my-3">
-                <div class="card-header red">
-                    <div class="icon-calendar-1 d-inline"></div> Termine
-                </div>
-                <div class="card-block text-center">
-                    <div class="icon-folder"></div> Projekt 1
-                </div>
-
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div class="card my-3">
-                <div class="card-header red">
-                    <div class="icon-star d-inline"></div> Favoriten
-                </div>
-                <div class="card-block text-center">
-                    <div class="icon-folder"></div> Projekt 1
-                </div>
-
+            <div class="col-3 text-right align-self-center">
+                <div class="icon-cog d-inline"></div> Einstellungen
             </div>
         </div>
     </div>
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="card my-3">
+                    <div class="card-header red">
+                        Projekte
+                    </div>
+                    <div class="card-block pre-scrollable pt-0">
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="col-7">
+                                    <div class="icon-folder d-inline"></div> Projekt 1
+                                </div>
+                                <div class="col-5 text-right">
+                                    07.04.2017
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="col-7">
+                                    <div class="icon-folder d-inline"></div> Projekt 2
+                                </div>
+                                <div class="col-5 text-right">
+                                    02.04.2017
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="col-7">
+                                    <div class="icon-folder d-inline"></div> Projekt 2
+                                </div>
+                                <div class="col-5 text-right">
+                                    02.04.2017
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="col-7">
+                                    <div class="icon-folder d-inline"></div> Projekt 2
+                                </div>
+                                <div class="col-5 text-right">
+                                    02.04.2017
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="col-7">
+                                    <div class="icon-folder d-inline"></div> Projekt 2
+                                </div>
+                                <div class="col-5 text-right">
+                                    02.04.2017
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card my-3">
+                    <div class="card-header red">
+                        Zuletzt geöffnete Dokumente
+                    </div>
+                    <div class="card-block pre-scrollable pt-0">
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="col-7">
+                                    <div class="icon-file-text d-inline"></div> Dokument 1
+                                </div>
+                                <div class="col-5 text-right">
+                                    07.04.2017
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="col-7">
+                                    <div class="icon-file-text d-inline"></div> Dokument 2
+                                </div>
+                                <div class="col-5 text-right">
+                                    02.04.2017
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="col-7">
+                                    <div class="icon-file-text d-inline"></div> Dokument 2
+                                </div>
+                                <div class="col-5 text-right">
+                                    02.04.2017
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card my-3">
+                    <div class="card-header red">
+                        <div class="icon-calendar-1 d-inline"></div> Termine
+                    </div>
+                    <div class="card-block text-center">
+                        <div class="icon-folder"></div> Projekt 1
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card my-3">
+                    <div class="card-header red">
+                        <div class="icon-star d-inline"></div> Favoriten
+                    </div>
+                    <div class="card-block text-center">
+                        <div class="icon-folder"></div> Projekt 1
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 <script src="js/jquery-3.2.0.min.js"></script>
@@ -123,7 +167,7 @@
 <script src="js/bootstrap.min.js"></script>
 <script>
     $(function(){
-            $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="tooltip"]').tooltip();
     });
 </script>
 </body>
