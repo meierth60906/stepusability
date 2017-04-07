f9658b2984bce09ba19411c44fa71df5e422e7e0
meierth60906 <meier.t96@gmail.com>
2017-04-07 18:54:25 +0200

    Dashboard: Farbige Icons, kleinere Korrekturen, Padding-Correction

--- home.php
+++ home.php
@@ -11,14 +11,14 @@
 
 <section id="dashboard" class="page-content">
     <div class="container-fluid">
-        <div class="row titlebar px-5 py-3">
-            <div class="col-8 align-self-center">
+        <div class="row titlebar px-3 py-3">
+            <div class="col-8">
                 <h1>Home</h1>
 
                 <!--            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>-->
             </div>
             <div class="col-4 text-right align-self-center ">
-                <a href="settings.php"><span class="icon-cog d-inline"></span> Einstellungen</a>
+                <a class="titlebar-link" href="settings.php"><div class="icon-cog d-inline-block px-2"></div>Einstellungen</a>
             </div>
         </div>
     </div>
@@ -32,55 +32,70 @@
                     <div class="card-block pre-scrollable pt-0">
                         <a class="card-item-link" href="home.php">
                             <div class="row card-item p-3">
-                                <div class="col-7">
-                                    <div class="icon-folder d-inline"></div> Projekt 1
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-folder d-inline text-muted"></div>
                                 </div>
-                                <div class="col-5 text-right">
-                                    07.04.2017
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Projekt 1
                                 </div>
-                            </div>
-                        </a>
-                        <hr>
-                        <a class="card-item-link" href="home.php">
-                            <div class="row card-item p-3">
-                                <div class="col-7">
-                                    <div class="icon-folder d-inline"></div> Projekt 2
-                                </div>
-                                <div class="col-5 text-right">
-                                    02.04.2017
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="text-muted">07.04.2017</div>
                                 </div>
                             </div>
                         </a>
                         <hr>
                         <a class="card-item-link" href="home.php">
                             <div class="row card-item p-3">
-                                <div class="col-7">
-                                    <div class="icon-folder d-inline"></div> Projekt 2
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-folder d-inline text-muted"></div>
                                 </div>
-                                <div class="col-5 text-right">
-                                    02.04.2017
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Projekt 2
                                 </div>
-                            </div>
-                        </a>
-                        <hr>
-                        <a class="card-item-link" href="home.php">
-                            <div class="row card-item p-3">
-                                <div class="col-7">
-                                    <div class="icon-folder d-inline"></div> Projekt 2
-                                </div>
-                                <div class="col-5 text-right">
-                                    02.04.2017
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="text-muted">07.04.2017</div>
                                 </div>
                             </div>
                         </a>
                         <hr>
                         <a class="card-item-link" href="home.php">
                             <div class="row card-item p-3">
-                                <div class="col-7">
-                                    <div class="icon-folder d-inline"></div> Projekt 2
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-folder d-inline text-muted"></div>
                                 </div>
-                                <div class="col-5 text-right">
-                                    02.04.2017
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Projekt 3
+                                </div>
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="text-muted">07.04.2017</div>
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-folder d-inline text-muted"></div>
+                                </div>
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Projekt 4
+                                </div>
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="text-muted">07.04.2017</div>
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-folder d-inline text-muted"></div>
+                                </div>
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Projekt 5
+                                </div>
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="text-muted">07.04.2017</div>
                                 </div>
                             </div>
                         </a>
@@ -98,40 +113,77 @@
                         <a class="card-item-link" href="home.php">
                             <div class="row card-item p-3">
                                 <div class="card-item-icon col-1">
-                                    <div class="icon-file-text"></div>
+                                    <div class="icon-file-text d-inline c-bericht"></div>
                                 </div>
-                                <div class="col-6">
-                                    Dokument 1
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Testbericht
                                     <div class="text-muted small">Projekt 1</div>
                                 </div>
-                                <div class="col-5 text-right">
-                                    07.04.2017
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="doc-date text-muted">07.04.2017</div>
                                 </div>
                             </div>
                         </a>
                         <hr>
                         <a class="card-item-link" href="home.php">
                             <div class="row card-item p-3">
-                                <div class="col-7">
-                                    <div class="icon-file-text d-inline"></div> Dokument 2
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-file-text d-inline c-plan"></div>
                                 </div>
-                                <div class="col-5 text-right">
-                                    02.04.2017
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Testplan
+                                    <div class="text-muted small">Projekt 1</div>
+                                </div>
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="doc-date text-muted">07.04.2017</div>
                                 </div>
                             </div>
                         </a>
                         <hr>
                         <a class="card-item-link" href="home.php">
                             <div class="row card-item p-3">
-                                <div class="col-7">
-                                    <div class="icon-file-text d-inline"></div> Dokument 2
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-file-text d-inline c-skript"></div>
                                 </div>
-                                <div class="col-5 text-right">
-                                    02.04.2017
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Testskript
+                                    <div class="text-muted small">Projekt 1</div>
+                                </div>
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="doc-date text-muted">07.04.2017</div>
                                 </div>
                             </div>
                         </a>
                         <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-file-text d-inline c-einverst"></div>
+                                </div>
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Einverständniserklärung
+                                    <div class="text-muted small">Projekt 1</div>
+                                </div>
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="doc-date text-muted">07.04.2017</div>
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="card-item-link" href="home.php">
+                            <div class="row card-item p-3">
+                                <div class="card-item-icon col-1">
+                                    <div class="icon-file-text d-inline c-protokoll"></div>
+                                </div>
+                                <div class="col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0">
+                                    Protokoll
+                                    <div class="text-muted small">Projekt 1</div>
+                                </div>
+                                <div class="col-4 hidden-xs-down text-right">
+                                    <div class="doc-date text-muted">07.04.2017</div>
+                                </div>
+                            </div>
+                        </a>
 
                     </div>
 
