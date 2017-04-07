eb5ade450f2284f58094e301aa9f02d0067c7ed9
meierth60906 <meier.t96@gmail.com>
2017-04-07 09:58:20 +0200

    Dashboard Grundgerüst angefangen

--- home.php
+++ home.php
@@ -6,6 +6,7 @@
 
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/custom.css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 </head>
 <body>
 
@@ -13,11 +14,60 @@
 
 <section id="dashboard">
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-12">
-            <h1>Step, the best tool to support usability testing.</h1>
+    <div class="row titlebar px-5 py-3">
+        <div class="col-lg-10 align-self-center">
+            <h1>Home</h1>
 
-            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+            <!--            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>-->
+        </div>
+        <div class="col-lg-2 text-right align-self-center">
+            <div class="icon-cog d-inline"></div> Einstellungen
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card m-5">
+                <div class="card-header red">
+                    Projekte
+                </div>
+                <div class="card-block text-center">
+                    <div class="icon-folder"></div> Projekt 1
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card m-5">
+                <div class="card-header red">
+                    Projekte
+                </div>
+                <div class="card-block text-center">
+                    <div class="icon-folder"></div> Projekt 1
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card m-5">
+                <div class="card-header red">
+                    Projekte
+                </div>
+                <div class="card-block text-center">
+                    <div class="icon-folder"></div> Projekt 1
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card m-5">
+                <div class="card-header red">
+                    Projekte
+                </div>
+                <div class="card-block text-center">
+                    <div class="icon-folder"></div> Projekt 1
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
