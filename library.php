898b2e51d4a1981e6d58c3a2ca89ca4b4b0ad77c
meierth60906 <meier.t96@gmail.com>
2017-04-09 14:27:38 +0200

    Seiten für Projektauswahl, Bibliothek und Termine erstellt. --> Erstellen von Branches

---
+++ library.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: meier
+ * Date: 09.04.2017
+ * Time: 14:25
+ */
---
+++ projects/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Projekte | step</title>
+
+    <?php include ('../head.html'); ?>
+</head>
+<body>
+
+<?php include ('../navigation.html'); ?>
+
+<section id="projects" class="page-content">
+    <div class="container-fluid">
+        <div class="row titlebar px-3 py-3">
+            <div class="col-8">
+                <h1>Projekte</h1>
+
+                <!--            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>-->
+            </div>
+            <div class="col-4 text-right align-self-center ">
+                <a class="titlebar-link" href="../settings.php">
+                    <div class="icon-cog d-inline-block px-2"></div>
+                    <span class="hidden-sm-down">Einstellungen</span></a>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<?php include ('../scripts.html'); ?>
+</body>
+
+</html>
