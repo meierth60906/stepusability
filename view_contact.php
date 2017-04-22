86b269e4eb42a59703675fd6eec1c4d5f9b13490
scharkal60932 <13Manu10>
2017-04-22 14:34:20 +0200

    Neuer Kontakt soweit fertig Kontakt bearbeiten und Ansicht muss ich noch überarbeiten Versuch die Buchstaben in die Kontaktliste mit einzubinden

--- view_contact.php
+++ view_contact.php
@@ -15,7 +15,7 @@
             <h1>Kontakte</h1>
         </div>
         <div class="col-4 text-right align-self-center ">
-            <a class="titlebar-link" href="view_contact.php">
+            <a class="titlebar-link" href="edit_contact.php">
                 <div class="icon-check d-inline-block px-2"></div>
                 <span class="hidden-sm-down">Speichern</span></a>
         </div>
@@ -62,11 +62,10 @@
                 </div>
             </div>-->
             <!-- Kontaktliste -->
-
-            <a class="row card-item p-3">A</a>
+            <a href="#" class="header">A</a>
             <a class="row card-item p-3 nav-link" href="#">Alena Ahorn</a>
             <a class="row card-item p-3 nav-link" href="#">Alessa Albert</a>
-            <a class="row card-item p-3 nav-link" href="#">Anna Apfel</a>
+            <a class="row card-item p-3 nav-link active" href="#">Anna Apfel</a>
             <a href="#" class="header">B</a>
             <a class="row card-item p-3 nav-link" href="#">Benjamin Berger</a>
             <a class="row card-item p-3 nav-link" href="#">Bernd Bogner</a>
@@ -105,9 +104,9 @@
                     <img class="img-circle" src="img/add_picture.png" />
                 </div>
                 <div class="col-lg-4">
-                    <input type="text" class="form-control" id="inputFirstName" placeholder="Vorname">
-                    <input type="text" class="form-control" id="inputName" placeholder="Nachname">
-                    <input type="text" class="form-control" id="inputName" placeholder="Firma">
+                    <input type="text" class="form-control" id="inputFirstName" value="Anna" placeholder="Vorname">
+                    <input type="text" class="form-control" id="inputName" value="Apfel" placeholder="Nachname">
+                    <input type="text" class="form-control" id="inputName" value="Apple" placeholder="Firma">
 
                 </div>
             </div>
@@ -115,51 +114,38 @@
             <div class="form-group row">
                 <label for="inputEmail3" class="col-lg-3 form-control-label">Kontaktmöglichkeiten</label>
                 <div class="col-lg-5">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="E-Mail">
-                    <input type="text" class="form-control" id="inputPhone" placeholder="Mobil">
+                    <input class="form-control" type="email" value="anna.apfel@apple.com" id="inputEmail3" placeholder="E-Mail">
+                    <input class="form-control" type="text" value="012345667" id="inputPhone" placeholder="Mobil">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="inputInfo" class="col-lg-3 form-control-label">Geburtsdatum</label>
+                <label for="inputBirthday" class="col-lg-3 form-control-label">Geburtsdatum</label>
                 <div class="col-lg-5">
-                    <input type="date" class="form-control" id="inputBirthday" placeholder="Geburtstag">
+                    <input class="form-control" type="date"  id="inputBirthday" value="01.01.1990" placeholder="Geburtstag">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="inputInfo" class="col-lg-3 form-control-label">Fähigkeiten</label>
+                <label for="inputSkill" class="col-lg-3 form-control-label">Fähigkeiten</label>
                 <div class="col-lg-5">
                     <div class="chip">
-                        <input type="text" class="card-text" id="inputSkill" placeholder="Skill 1">
-                        <input type="text" class="card-text" id="inputSkill" placeholder="Skill 2">
-                        <input type="text" class="card-text" id="inputSkill" placeholder="Skill 3">
+                        <input type="text" class="card-text" id="inputSkill" value="Java" placeholder="Skill 1">
+                        <input type="text" class="card-text" id="inputSkill" value="Android" placeholder="Skill 2">
+                        <input type="text" class="card-text" id="inputSkill" value="CSS" placeholder="Skill 3">
                     </div>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="inputAddress" class="col-lg-3 form-control-label">Anschrift</label>
                 <div class="col-lg-5">
-                    <input type="text" class="form-control" id="inputAddress" placeholder="Straße & Hausnr.">
-                    <input type="text" class="form-control" id="inputAddress" placeholder="PLZ">
-                    <input type="text" class="form-control" id="inputName" placeholder="Ort">
+                    <input type="text" class="form-control" id="inputAddress" value="Ahornstraße 22" placeholder="Straße & Hausnr.">
+                    <input type="text" class="form-control" id="inputAddress" value="12345" placeholder="PLZ">
+                    <input type="text" class="form-control" id="inputAddress" value="Nürnberg" placeholder="Ort">
                 </div>
             </div>
             <div class="modal-footer text-center">
                 <button type="button" class="btn btn-submit-orange btn-md">Kontakt löschen</button>
             </div>
     </div>
-    <!--<div class="row titlebar px-3 py-3">
-        <div class="col-8">
-            <h1>Kontakte</h1>
-        </div>
-
-        <div class="col-4 text-right align-self-center ">
-            <a class="titlebar-link" href="#">
-                <div class="icon-check d-inline-block px-2"></div>
-                <span class="hidden-sm-down">Speichern</span></a>
-        </div>
-    </div>
-            <!--innere Navigationsleiste-->
-
 
 
 
