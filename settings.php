b19028ce86e115df12b8a60f8f79f2756adae728
Isabel Eichner <eichneris60139@th-nuernberg.de>
2017-05-02 20:17:45 +0200

    templates to navbar added

--- settings.php
+++ settings.php
@@ -22,7 +22,7 @@
             <div class="col-lg-3 p-0" id="secondSidebar">
                 <ul class="flex-column nav">
                     <li class="nav-item">
-                        <a class="p-3 nav-link" href="#profile" role="tab" data-toggle="tab">Profil
+                        <a class="p-3 nav-link active" href="#profile" role="tab" data-toggle="tab">Profil
                             bearbeiten</a></li>
                     <li class="nav-item">
                         <a class="p-3 nav-link" href="#password" role="tab" data-toggle="tab">Passwort ändern</a></li>
@@ -39,137 +39,155 @@
 
             <!--            Profil bearbeiten Seite -->
 
-            <div class="tab-pane row" id="profile" role="tabpanel">
+            <div class="tab-pane active" id="profile" role="tabpanel">
+                <div class="row">
+                    <div class="form-group pt-5 col-lg-8 offset-lg-4">
+                        <form>
+                            <div class="form-group row">
+                                <div class="profile-header-img">
+                                    <img class="img-circle" src="img/add_picture.png"/>
+                                </div>
+                                <div class="col-lg-4">
+                                    <input type="text" class="form-control" id="vorname"
+                                           placeholder="Vorname">
+                                    <input type="text" class="form-control" id="nachname"
+                                           placeholder="Nachname">
+                                    <input type="text" class="form-control" id="firma"
+                                           placeholder="Firma">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="email" class="col-lg-3 form-control-label">Kontaktmöglichkeiten</label>
+                                <div class="col-lg-5">
+                                    <input type="email" class="form-control" id="email" placeholder="E-Mail">
+                                    <input type="text" class="form-control" id="phone" placeholder="Mobil">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="birthday" class="col-lg-3 form-control-label">Geburtsdatum</label>
+                                <div class="col-lg-5">
+                                    <input type="date" class="form-control" id="birthday" placeholder="Geburtstag">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="skill" class="col-lg-3 form-control-label">Fähigkeiten</label>
+                                <div class="col-lg-5">
+                                    <div class="chip">
+                                        <input type="text" class="card-text" id="skill" placeholder="Skill 1">
+                                        <input type="text" class="card-text" id="skill" placeholder="Skill 2">
+                                        <input type="text" class="card-text" id="skill" placeholder="Skill 3">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputAddress" class="col-lg-3 form-control-label">Anschrift</label>
+                                <div class="col-lg-5">
+                                    <input type="text" class="form-control" id="address"
+                                           placeholder="Straße & Hausnr.">
+                                    <input type="text" class="form-control" id="address" placeholder="PLZ">
+                                    <input type="text" class="form-control" id="place" placeholder="Ort">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="offset-lg-3 col-lg-5">
+                                    <div class="modal-footer text-center">
+                                        <button type="button" class="btn btn-submit-orange btn-md">Speichern</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+
+            <!--                    Passwort ändern seite-->
+
+
+            <div class="tab-pane row" id="password" role="tabpanel">
                 <div class="form-group pt-5 col-lg-8 offset-lg-4">
                     <form>
+
+                        <!--    akutelles Passwort eingeben-->
                         <div class="form-group row">
-                            <div class="profile-header-img">
-                                <img class="img-circle" src="img/add_picture.png"/>
-                            </div>
-                            <div class="col-lg-4">
-                                <input type="text" class="form-control" id="vorname"
-                                       placeholder="Vorname">
-                                <input type="text" class="form-control" id="nachname"
-                                       placeholder="Nachname">
-                                <input type="text" class="form-control" id="firma"
-                                       placeholder="Firma">
+                            <label for="currentPassword" class="col-lg-6 form-control-label">Bitte geben Sie Ihr
+                                aktuelles
+                                Passwort ein:</label>
+                            <div class="col-lg-5">
+                                <input type="text" class="form-control" id="currentPassword"
+                                       placeholder="aktuelles Passwort">
                             </div>
                         </div>
 
+                        <!--                            neues Passwort eingeben-->
+
                         <div class="form-group row">
-                            <label for="email" class="col-lg-3 form-control-label">Kontaktmöglichkeiten</label>
+                            <label for="newPassword" class="col-lg-6 form-control-label">Bitte geben Sie Ihr neues
+                                Passwort
+                                ein:</label>
                             <div class="col-lg-5">
-                                <input type="email" class="form-control" id="email" placeholder="E-Mail">
-                                <input type="text" class="form-control" id="phone" placeholder="Mobil">
+                                <input type="text" class="form-control" id="newPassword" placeholder="neues Passwort">
                             </div>
                         </div>
+
+                        <!--                            Passwort wiederholen-->
+
                         <div class="form-group row">
-                            <label for="birthday" class="col-lg-3 form-control-label">Geburtsdatum</label>
-                            <div class="col-lg-5">
-                                <input type="date" class="form-control" id="birthday" placeholder="Geburtstag">
+                            <div class="col-lg-5 offset-6">
+                                <input type="text" class="form-control" id="repeatPassword"
+                                       placeholder="Passwort wiederholen">
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="skill" class="col-lg-3 form-control-label">Fähigkeiten</label>
-                            <div class="col-lg-5">
-                                <div class="chip">
-                                    <input type="text" class="card-text" id="skill" placeholder="Skill 1">
-                                    <input type="text" class="card-text" id="skill" placeholder="Skill 2">
-                                    <input type="text" class="card-text" id="skill" placeholder="Skill 3">
+
+                        <!--                         Passwort ändern Button -->
+                        <div class="row">
+                            <div class="col-lg-5 offset-6">
+                                <div class="text-right">
+                                    <button id="buttonPassword" type="button" class="btn btn-submit-orange btn-md">Passwort speichern</button>
                                 </div>
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="inputAddress" class="col-lg-3 form-control-label">Anschrift</label>
-                            <div class="col-lg-5">
-                                <input type="text" class="form-control" id="address"
-                                       placeholder="Straße & Hausnr.">
-                                <input type="text" class="form-control" id="address" placeholder="PLZ">
-                                <input type="text" class="form-control" id="place" placeholder="Ort">
-                            </div>
-                        </div>
-                        <div class="modal-footer text-center">
-                            <button type="button" class="btn btn-submit-orange btn-md">Speichern</button>
-                        </div>
+
                     </form>
                 </div>
+            </div>
 
+            <!--            pageInformation-->
+
+
+            <div class="tab-pane" id="pageInfo" role="tabpanel">
+                <div class="form-group pt-5 col-lg-8 offset-lg-4">
+                    <p class="font-weight-bold text-center">Datenschutz</p>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                        ut labore
+                        et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                        ea
+                        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                        ipsum
+                        dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                        dolore
+                        magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet
+                        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    </p>
+                </div>
             </div>
 
         </div>
     </div>
 
-    <!--                    Passwort ändern seite-->
-
-
-    <div class="tab-pane row" id="password" role="tabpanel">
-        <div class="form-group pt-5 col-lg-8 offset-lg-4">
-            <form>
-
-                <!--    akutelles Passwort eingeben-->
-                <div class="form-group row">
-                    <label for="currentPassword" class="col-lg-6 form-control-label">Bitte geben Sie Ihr aktuelles
-                        Passwort ein:</label>
-                    <div class="col-lg-5">
-                        <input type="text" class="form-control" id="currentPassword" placeholder="aktuelles Passwort">
-                    </div>
-                </div>
-
-                <!--                            neues Passwort eingeben-->
-
-                <div class="form-group row">
-                    <label for="newPassword" class="col-lg-6 form-control-label">Bitte geben Sie Ihr neues Passwort
-                        ein:</label>
-                    <div class="col-lg-5">
-                        <input type="text" class="form-control" id="newPassword" placeholder="neues Passwort">
-                    </div>
-                </div>
-
-                <!--                            Passwort wiederholen-->
-
-                <div class="form-group row">
-                    <div class="col-lg-5 offset-6">
-                        <input type="text" class="form-control" id="repeatPassword"
-                               placeholder="Passwort wiederholen">
-                    </div>
-                </div>
-
-                <!--                         Passwort ändern Button -->
-
-                <div class="modal-footer text-center">
-                    <button type="button" class="btn btn-submit-orange btn-md">Passwort speichern</button>
-                </div>
-            </form>
-        </div>
-    </div>
-
-    <!--            pageInformation-->
-
-
-    <div class="tab-pane" id="pageInfo" role="tabpanel">
-        <div class="form-group pt-5 col-lg-8 offset-lg-4">
-            <p class="font-weight-bold text-center">Datenschutz</p>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-                et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-                rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-                clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
-        </div>
-    </div>
 
 </section>
 
 <?php include('scripts.html'); ?>
 <script>
-    $(function(){
+    $(function () {
         $('.nav').click(function (e) {
             e.preventDefault()
             $(this).tab('show')
         })
     });
-
 
 
 </script>
