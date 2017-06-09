222eadd232ad2e58bb79c305dced513a9ef56737
meierth60906 <meier.t96@gmail.com>
2017-06-09 12:05:32 +0200

    Testaufgaben vorab Push

--- logic/selectProjectlistDashboard.php
+++ logic/selectProjectlistDashboard.php
@@ -13,17 +13,17 @@ $fetchRowCount = oci_fetch_row($stid);
 
 if($fetchRowCount) {
     while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
-        echo "<a class='card-item-link' href='projects/project.php?id=" . $row['ID'] . "'>
+        echo "<a class='card-item-link' href='projects/project.php?id=" . $fetchRowCount[0] . "'>
                             <div class='row card-item p-3'>
                                 <div class='card-item-icon col-1 middle-two-line'>
                                     <div class='icon-folder d-inline text-muted'></div>
                                 </div>
                                 <div class='col-7 pl-sm-1 pl-md-0 pl-lg-2 pl-xl-0 middle-two-line'>
-                                    ". $row['NAME'] ."
+                                    ". $fetchRowCount[1] ."
                                 </div>
                                 <div class='col-4 hidden-xs-down text-right'>
                                     <div class='text-muted small'>Letzte Änderung am</div>
-                                    <div class='text-muted'>". $row['ZULETZT_GEAENDERT'] ."</div>
+                                    <div class='text-muted'>". $fetchRowCount[5] ."</div>
                                 </div>
                             </div>
                         </a>
