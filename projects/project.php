b50330a3a40a0c473ae823e32a714e5e1a6b5b68
scharkal60932 <13Manu10>
2017-07-21 19:17:41 +0200

    Editor Eval Versuch

--- projects/project.php
+++ projects/project.php
@@ -786,7 +786,7 @@ if (!isset($_GET['id'])) {
                                     </div>
 
                                 <div class="loesungsfeld">
-                                    <div class="col-10 p-3">
+                                    <div class="col-12 p-3">
                                         <table class="table table-bordered">
                                             <tr>
                                                 <td>
@@ -834,7 +834,7 @@ if (!isset($_GET['id'])) {
                                         </div>
                                     </div>
                                 <div class="loesungsfeld">
-                                    <div class="col-10 p-3">
+                                    <div class="col-12 p-3">
                                         <table class="table table-bordered">
 
                                             <tr>
@@ -885,7 +885,7 @@ if (!isset($_GET['id'])) {
                                         </div>
                                     </div>
                                 <div class="loesungsfeld">
-                                    <div class="col-10 p-3">
+                                    <div class="col-12 p-3">
                                         <table class="table table-bordered">
 
                                             <tr>
