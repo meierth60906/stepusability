0ea1bfb1c4de38633f4caa35f277331c7ba73f8b
Isabel Eichner <eichneris60139@th-nuernberg.de>
2017-07-23 15:55:03 +0200

    fixed

--- projects/project.php
+++ projects/project.php
@@ -1789,24 +1789,6 @@ function createAgreement() {
             "</ol>" +
             "</li>";
 
-        <?php
-
-        $conn = oci_connect('studi131', 'studi131', '//dbcluster.cs.ohm-hochschule.de:1521/oracle.ohmhs.de');
-        if (!$conn) {
-            $e = oci_error();
-            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-        }
-
-        $stid = oci_parse($conn, "SELECT * FROM VORLAGE WHERE VORLAGE_ART='Einverstaendniserklaerung'");
-        oci_execute($stid);
-        while($row=oci_fetch_array($stid,OCI_ASSOC+OCI_RETURN_NULLS)){
-            $content = $row['VORLAGE_TEXT']->load();
-            $content = htmlspecialchars($content);
-            echo "<li><a class=\"icon-file-text-o p-3 nav-link active\" href=\"javascript:ShowTemplate('".$row['VORLAGE_ART']."', '".$row['VORLAGE_NAME']."', '".$content."');\">".$row['VORLAGE_NAME']."</a></li>";
-        }
-        ?>
-
-
     }
 
     if(!(agreementContainer.find('.agreementRubrik').length !== 0)) {
