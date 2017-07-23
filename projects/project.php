7f639863393aa21b9396c65c22288bd5837ad9f0
scharkal60932 <13Manu10>
2017-07-23 20:12:39 +0200

    Evaluationsfenster Editor

--- projects/project.php
+++ projects/project.php
@@ -2254,6 +2254,7 @@ function createLoeswegNew(event) {
         branding: false,
         plugins: 'table',
         resize: 'both',
+        body_id: 'editor',
         content_css: [
             '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
             '//www.tinymce.com/css/codepen.min.css'],
