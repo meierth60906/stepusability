0aaa7f2d6567304c90987d1dc1dea015b514f9d6
Isabel Eichner <eichneris60139@th-nuernberg.de>
2017-07-23 15:27:28 +0200

    test

--- projects/agreementForm.php
+++ projects/agreementForm.php
@@ -20,24 +20,3 @@
         }
         ?>
 </div>
-
-    <div class="container-fluid">
-        <div class="tab-content">
-
-
-            <div class="tab-pane active" id="einverstaendniserklaerung1" role="tabpanel">
-                <div class="row">
-                    <div class="col-lg-8 offset-lg-4 editor">
-                        <form id="templateForm" class="form" action="logic/insertTemplate.php" method="post">
-                            <button class="visibility" name="submitbtn"></button>
-                            <p class="visibility" name="Name_der_Vorlage" id="Name_der_Vorlage">Name der Vorlage:</p>
-                            <input class="visibility" name="Vorlage_Name" type="text" id="Vorlage_Name" placeholder="Name der Vorlage">
-                            <input type="hidden" name="Vorlage_Art" id="Vorlage_Art">
-                            <textarea class="visibility" name="Vorlage_Text" class="editor" id="Vorlage_Text">
-                            </textarea>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
