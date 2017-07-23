2e7c8f73ebd5a5cb3658b893d4251cd0e8d91a00
scharkal60932 <13Manu10>
2017-07-23 20:26:02 +0200

    Evaluationsfenster Editor

--- projects/project.php
+++ projects/project.php
@@ -906,16 +906,14 @@ if (!isset($_GET['id'])) {
                                         <div class="row px-4">
                                             <div class="col-lg-4">Lösungsschritte:</div>
                                             <div class="col-lg-8 font-weight-bold">
-                                                Auf Dashboard geht es nicht -->Navigationsleiste auf Projekte - Übersicht aller Projekte - erstellen - Namen eingeben
+                                                Auf Dashboard geht es nicht > Navigationsleiste auf Projekte - Übersicht aller Projekte - erstellen - Namen eingeben
 
                                             </div>
                                         </div>
                                         <div class="row px-4 p-4">
                                             <div class="col-lg-4">Anmerkungen:</div>
                                             <div class="col-lg-8 font-weight-bold">
-                                                Lorem ipsum dolor sit amet, consetetur
-                                                sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                                                magna aliquyam erat, sed diam voluptua.
+
                                             </div>
                                         </div>
                                     </div>
