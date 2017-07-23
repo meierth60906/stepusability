27a8400895d94af5081d3bee43db8f1832a4be3b
scharkal60932 <13Manu10>
2017-07-23 20:32:28 +0200

    Evaluationsfenster Editor

--- projects/project.php
+++ projects/project.php
@@ -925,9 +925,6 @@ if (!isset($_GET['id'])) {
 
 
                         <textarea id="editor">
-                      <p>
-                        <img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" />
-                      </p>
 
                       <h1>Testbericht</h1>
 
