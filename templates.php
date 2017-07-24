537dbed86616fc29f3599146bfc6e0de21d22422
Maura <maierma60902@th-nuernberg.de>
2017-07-24 19:58:04 +0200

    studi 131 -> studi 132

--- templates.php
+++ templates.php
@@ -98,7 +98,7 @@
 
                                 <?php
 
-                                $conn = oci_connect('studi131', 'studi131', '//dbcluster.cs.ohm-hochschule.de:1521/oracle.ohmhs.de');
+                                $conn = oci_connect('studi132', 'studi132', '//dbcluster.cs.ohm-hochschule.de:1521/oracle.ohmhs.de');
                                 if (!$conn) {
                                     $e = oci_error();
                                     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
