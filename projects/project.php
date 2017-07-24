41c86c8398e11d9b3617d40bcc51e37271bccd96
Isabel Eichner <eichneris60139@th-nuernberg.de>
2017-07-24 17:38:09 +0200

    editor changed

--- projects/project.php
+++ projects/project.php
@@ -2255,7 +2255,23 @@ function createLoeswegNew(event) {
         contentUnterlagen.load("agreementForm.php");
     }
     function openEditor() {
-        contentUnterlagen.load("editor.js");
+        tinymce.init({
+            selector: 'textarea',
+            height: 500,
+            theme: 'modern',
+            plugins: [
+                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen',
+                'insertdatetime media nonbreaking save table contextmenu directionality',
+                'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'
+            ],
+            toolbar1: 'save | undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
+            image_advtab: true,            content_css: 'https://tinymce.com/css/codepen.min.css',
+            branding: false,
+            language_url : '/step/tiny/mce/langs/de.js',
+            save_enablewhendirty: true
+        });
     }
     function editProtocol() {
         contentUnterlagen.load("protocolForm.html");
