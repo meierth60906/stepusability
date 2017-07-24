b9d5b842d8425ea0916d813d8fdaaaec453f1296
scharkal60932 <13Manu10>
2017-07-24 17:57:00 +0200

    editor eval

--- projects/project.php
+++ projects/project.php
@@ -2256,25 +2256,23 @@ if (!isset($_GET['id'])) {
         contentUnterlagen.load("agreementForm.php");
     }
     function openEditor() {
-        contentUnterlagen.load("editor.html");
-
-//        tinymce.init({
-//            selector: 'textarea',
-//            height: 500,
-//            theme: 'modern',
-//            plugins: [
-//                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-//                'searchreplace wordcount visualblocks visualchars code fullscreen',
-//                'insertdatetime media nonbreaking save table contextmenu directionality',
-//                'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'
-//            ],
-//            toolbar1: 'save | undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-//            toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
-//            image_advtab: true,            content_css: 'https://tinymce.com/css/codepen.min.css',
-//            branding: false,
-//            language_url : '/step/tiny/mce/langs/de.js',
-//            save_enablewhendirty: true
-//        });
+        tinymce.init({
+            selector: 'textarea',
+            height: 500,
+            theme: 'modern',
+            plugins: [
+                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen',
+                'insertdatetime media nonbreaking save table contextmenu directionality',
+                'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'
+            ],
+            toolbar1: 'save | undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
+            image_advtab: true,            content_css: 'https://tinymce.com/css/codepen.min.css',
+            branding: false,
+            language_url : '/step/tiny/mce/langs/de.js',
+            save_enablewhendirty: true
+        });
     }
     function editProtocol() {
         contentUnterlagen.load("protocolForm.html");
@@ -2300,7 +2298,7 @@ if (!isset($_GET['id'])) {
 <script>
 
     tinymce.init({
-        selector: 'editor',
+        selector: '.editor',
         height: 500,
         branding: false,
         plugins: 'table',
